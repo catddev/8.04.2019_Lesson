85a9345ff99084355571e36d488256a12675e6cc
айкеновад <@205_8>
2019-04-08 09:23:52 +0600

    Добавьте файлы проекта.

---
+++ 08.04.2019_Lesson/Point.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+class Point {
+private:
+	double x, y;
+public:
+	Point() :x(0), y(0) {}
+	Point(double x, double y) {
+		this->x = x;
+		this->y = y;
+	}
+	void setX(double x) { this->x = x; }
+	void setY(double y) { this->y = y; }
+
+	double getX()const { return x; }
+	double getY()const { return y; }
+};
