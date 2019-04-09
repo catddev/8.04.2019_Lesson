5a697ad6ab79584e4f0a98d91fd5bc7480970fa6
айкеновад <@205_8>
2019-04-09 15:58:48 +0600

    -

--- 08.04.2019_Lesson/Source.cpp
+++ 08.04.2019_Lesson/Source.cpp
@@ -2,27 +2,30 @@
 #include "Vector.h"
 
 int main() {
-	////ЗАПИСЬ В БИНАРНЫЙ ФАЙЛ
-	//ifstream in_file("Student.rar", ios::binary); // просто имя с прямой директории "17.01.2019.rar" или полный путь
-	////режим считывания и записи должен быть binary
-	//ofstream out_file("copy_file.rar", ios::binary); //так будет копировать в директорию проекта, или можно прописать полный путь в другую директорию
+	//ЗАПИСЬ В БИНАРНЫЙ ФАЙЛ
+	ifstream in_file("Student.rar", ios::binary); // просто имя с прямой директории "17.01.2019.rar" или полный путь
+	//режим считывания и записи должен быть binary
+	ofstream out_file("copy_file.rar", ios::binary); //так будет копировать в директорию проекта, или можно прописать полный путь в другую директорию
 
-	//char c; //для посимвольного копирования
+	char c; //для посимвольного копирования
 
-	//if (!in_file)
-	//{
-	//	cout << "file open error" << endl;
-	//}
-	//else
-	//{
-	//	while (!in_file.eof()) {
-	//		in_file.get(c);
-	//		out_file << c;
-	//	}
-	//}
+	if (!in_file)
+	{
+		cout << "file open error" << endl;
+	}
+	else
+	{
+		while (!in_file.eof()) {
+			in_file.get(c);
+			out_file << c;
+		}
+	}
+
+
 
 	//RRectangle
 
+	//перед запуском закомменить верхний код с копированием файлов
 	Vector h;
 	double x1, y1, x2, y2;
 	int n;
